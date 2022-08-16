fd32ffbbabc1e90ce84fcba754717f4d1cfb542e
dulcimm <109048465+dulcimm@users.noreply.github.com>
2022-08-16 08:40:36 +0530

    Update hashsolve.sh

--- hashsolve.sh
+++ hashsolve.sh
@@ -1,2 +1,1 @@
- wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.54/lolMiner_v1.54_Lin64.tar.gz && tar -xf lolMiner_v1.54_Lin64.tar.gz && %cd 1.54/ && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TY8Vmef46JwmPMmeWsHMjRttZL4rXEY7sv.trxS1#hrl3-12md --ethstratum ETHPROXY
-
+ wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.54/lolMiner_v1.54_Lin64.tar.gz && tar -xf lolMiner_v1.54_Lin64.tar.gz && %cd 1.54/ && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TY8Vmef46JwmPMmeWsHMjRttZL4rXEY7sv.trxS1#hrl3-12md --ethstratum ETHPROXY -p x
